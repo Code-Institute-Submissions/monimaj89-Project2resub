5ca09d79ba4e38231801484815c6163adaddfa13
monimaj89 <mn.majchrowicz@gmail.com>
2023-03-24 20:35:10 +0000

    Add search function to map

--- assets/js/map.js
+++ assets/js/map.js
@@ -2,6 +2,7 @@ let map, places;
 let infoWindow;
 let markers = [];
 let autocomplete;
+let placeType;
 
 const MARKER_PATH =
   "https://developers.google.com/maps/documentation/javascript/images/marker_green";
@@ -44,3 +45,43 @@ const MARKER_PATH =
       document.getElementById("searchBox");
     }
   }
+
+  // Search for hotels, restaurant and attractions in the selected city, within the viewport of the map.
+
+function search() {
+  const search = {
+    bounds: map.getBounds(),
+    types: ["lodging"],
+  };
+
+  places.nearbySearch(search, (results, status, pagination) => {
+    if (status === google.maps.places.PlacesServiceStatus.OK && results) {
+      clearResults();
+      clearMarkers();
+
+      // Create a marker for each place found, and
+      // assign a letter of the alphabetic to each marker icon.
+
+      for (let i = 0; i < results.length; i++) {
+        const markerLetter = String.fromCharCode("A".charCodeAt(0) + (i % 26));
+        const markerIcon = MARKER_PATH + markerLetter + ".png";
+
+        // Use marker animation to drop the icons incrementally on the map.
+
+        markers[i] = new google.maps.Marker({
+          position: results[i].geometry.location,
+          animation: google.maps.Animation.DROP,
+          icon: markerIcon,
+        });
+        // If the user clicks a hotel marker, show the details of that hotel
+        // in an info window.
+        // @ts-ignore TODO refactor to avoid storing on marker
+        
+        markers[i].placeResult = results[i];
+        google.maps.event.addListener(markers[i], "click", showInfoWindow);
+        setTimeout(dropMarker(i), i * 100);
+        addResult(results[i], i);
+      }
+    }
+  });
+}
